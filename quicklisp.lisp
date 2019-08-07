f76f422f10ae35e17964dcf480a9b79492c0f2cd
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-08-07 14:51:00 -0400

    Add in-package to quicklisp.lisp

--- quicklisp.lisp
+++ quicklisp.lisp
@@ -1,9 +1,11 @@
+(in-package #:cl-user)
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (find :quicklisp *features*)
-    (let ((quicklisp-init (merge-pathnames (make-pathname
-                                            :directory '(:relative "quicklisp")
-                                            :name "setup"
-                                            :type "lisp")
-                                           (user-homedir-pathname))))
-      (when (probe-file quicklisp-init)
-        (load quicklisp-init)))))
+    (let ((#1=#:quicklisp-init (merge-pathnames (make-pathname
+                                                 :directory '(:relative "quicklisp")
+                                                 :name "setup"
+                                                 :type "lisp")
+                                                (user-homedir-pathname))))
+      (when (probe-file #1#)
+        (load #1#)))))
