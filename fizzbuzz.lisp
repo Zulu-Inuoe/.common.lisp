d1a4b4125d096b96258956a62853ef6ddbd9e35e
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-08-07 14:29:42 -0400

    Move fizzbuzz into own file

---
+++ fizzbuzz.lisp
@@ -0,0 +1,12 @@
+(in-package #:cl-user)
+
+(ql-import #:drakma #:jonathan)
+
+(defun fizzbuzz (n)
+  "Return \"Fizz\", \"Buzz\", \"FizzBuzz\", or \"`n'\" depending on the divisibility of `n'"
+  (cadr
+   (assoc "numbers"
+          (jonathan:parse
+           (drakma:http-request (format nil "https://itwont.work/fizzbuzz/api?start=~A&end=~:*~A" n))
+           :as :alist)
+          :test #'string=)))
--- utils.lisp
+++ utils.lisp
@@ -273,14 +273,4 @@ Each dependency is identified by its name (a string)."
       pathname)))
 
 (include "place")
-
-(ql-import #:drakma #:jonathan)
-
-(defun fizzbuzz (n)
-  "Return \"Fizz\", \"Buzz\", \"FizzBuzz\", or \"`n'\" depending on the divisibility of `n'"
-  (cadr
-   (assoc "numbers"
-          (jonathan:parse
-           (drakma:http-request (format nil "https://itwont.work/fizzbuzz/api?start=~A&end=~:*~A" n))
-           :as :alist)
-          :test #'string=)))
+(include "fizzbuzz")
