9abb1b8ae71bcb5566bb30159245fd3272976090
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-12-12 18:13:59 -0500

    Add MacOSX support for open-in-default-application

--- uiop+.lisp
+++ uiop+.lisp
@@ -3,8 +3,14 @@
 (import+ #:uiop)
 
 (defun open-in-default-application (pathname)
-  (if (uiop:os-windows-p)
-      (if (uiop:directory-pathname-p pathname)
+  (cond
+    ((uiop:os-windows-p)
+     (if (uiop:directory-pathname-p pathname)
           (uiop:launch-program (list "explorer" (uiop:native-namestring pathname)))
-          (uiop:launch-program (uiop:native-namestring pathname)))
-      (uiop:launch-program (list "xdg-open" (uiop:native-namestring pathname)))))
+          (uiop:launch-program (uiop:native-namestring pathname))))
+    ((uiop:os-macosx-p)
+     (uiop:launch-program (list "open" (uiop:native-namestring pathname))))
+    ((uiop:os-unix-p)
+     (uiop:launch-program (list "xdg-open" (uiop:native-namestring pathname))))
+    (t
+     (error "Don't know how to open in default application"))))
