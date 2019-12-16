906a87d5212969142c0a37305c04941a71854b06
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2019-12-16 17:42:16 -0500

    Minor indentation fix

--- uiop+.lisp
+++ uiop+.lisp
@@ -6,8 +6,8 @@
   (cond
     ((uiop:os-windows-p)
      (if (uiop:directory-pathname-p pathname)
-          (uiop:launch-program (list "explorer" (uiop:native-namestring pathname)))
-          (uiop:launch-program (uiop:native-namestring pathname))))
+         (uiop:launch-program (list "explorer" (uiop:native-namestring pathname)))
+         (uiop:launch-program (uiop:native-namestring pathname))))
     ((uiop:os-macosx-p)
      (uiop:launch-program (list "open" (uiop:native-namestring pathname))))
     ((uiop:os-unix-p)
