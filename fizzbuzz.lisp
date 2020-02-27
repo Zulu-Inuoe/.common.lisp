bf617dbb2b376ba335ddecb455b15583882f2b56
Wilfredo Velázquez-Rodríguez <zulu.inuoe@gmail.com>
2020-02-27 15:16:33 -0500

    use jsown rather than jonathan because that breaks on LW

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,12 +1,9 @@
 (in-package #:cl-user)
 
-(:import+ #:drakma #:jonathan)
+(:import+ #:drakma #:jsown)
 
 (defun :fizzbuzz (n)
   "Return \"Fizz\", \"Buzz\", \"FizzBuzz\", or \"`n'\" depending on the divisibility of `n'"
-  (cadr
-   (assoc "numbers"
-          (jonathan:parse
-           (drakma:http-request (format nil "https://itwont.work/fizzbuzz/api?start=~A&end=~:*~A" n))
-           :as :alist)
-          :test #'string=)))
+  (jsown:val-safe (jsown:parse
+                   (drakma:http-request (format nil "https://itwont.work/fizzbuzz/api?start=~A&end=~:*~A" n)))
+                  "numbers"))
